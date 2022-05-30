7adcd9223a7b0a4c71bbecefac0805b5ef2d3573
Diego Fagundes <60697333+diegofag@users.noreply.github.com>
2022-05-30 15:04:46 -0300

    Aula de comentários

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,16 @@
+// Comentário de 1 linha
+console.info('Linha 1')
+
+/*
+Comentário de 
+múltiplas 
+linhas
+*/
+console.info('Linhas múltiplas')
+
+/**
+ * Comentário de
+ * múltiplas 
+ * linhas 
+ */
+console.info('Linhas múltiplas')
